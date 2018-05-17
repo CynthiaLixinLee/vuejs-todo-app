e74ecf7bd8a76820b09706ed837637461a92ec6c
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-05-17 13:31:24 -0400

    Added editing functionality, and saving in localStorage

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@
 // import Vue from 'vue';
 
 // Vue.config.productionTip = false;
+const STORAGE_KEY = 'todo-storage';
+function saveToStorage(x) {
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(x));
+}
 
 new Vue({
   // entry point of the app
@@ -10,16 +14,49 @@ new Vue({
   data() {
     return {
       newTodo: '',
-      todos: [{ id: 0, title: 'New Todo', completed: 'false' }],
+      todos: [],
+      editedTodo: null,
+      visibility: 'all',
     };
+  },
+  // Vue.js lifecycle hook to load from localStorage
+  created() {
+    this.todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+  },
+  computed: {
+    filteredTodos() {
+      if(this.visibility === 'all') {
+        return this.todos;
+      } else if (this.visibility === 'active') {
+          return this.todos.filter(todo => !todo.completed)
+      } else {
+          return this.todos.filter(todo => todo.completed)
+      }
+    }
   },
   methods: {
     addTodo() {
       this.todos.push({ title: this.newTodo, completed: 'false', id: this.todos.length });
       this.newTodo = '';
+      saveToStorage(this.todos);
     },
-    removeTodo(todo){
-      this.todos.splice(this.todos.indexOf(todo),1)
-    }
+    editTodo(todo) {
+      this.editedTodo = todo;
+    },
+    doneEdit(todo) {
+      if (!this.editedTodo) {
+        return;
+      }
+      this.editedTodo = null;
+      todo.title = todo.title.trim();
+      if (!todo.title) {
+        this.removeTodo(todo);
+      }
+      saveToStorage(this.todos);
+    },
+    removeTodo(todo) {
+      this.todos.splice(this.todos.indexOf(todo), 1);
+      saveToStorage(this.todos);
+    },
   },
 });
