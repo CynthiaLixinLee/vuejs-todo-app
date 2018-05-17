3a6dd318c2a676f90bfda90580774a0b3d4beb36
Cynthia <29832823+CynthiaLixinLee@users.noreply.github.com>
2018-05-16 23:09:49 -0400

    FIrst commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <div class="header">To Do List</div>
+    <todo-list v-bind:todos="todos"></todo-list>
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  },
+  data() {
+    return {
+      todos: [
+        {
+          title: 'Study algorithms',
+          done: false,
+        },
+        {
+          title: 'Do research - Jest testing',
+          done: true,
+        },
+        {
+          title: 'Write Medium article',
+          done: false,
+        },
+        {
+          title: 'Listen to Code Newbie podcast',
+          done: false,
+        },
+      ],
+    };
+  },
+};
+</script>
